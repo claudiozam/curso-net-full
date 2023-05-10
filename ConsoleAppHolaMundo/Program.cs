1706750596ee198791f21ba7da152bbbdc455266
claudio z <claudio.zamoszczyk@gmail.com>
2023-05-10 15:03:43 -0300

    cambios para el primer commit

---
+++ ConsoleAppHolaMundo/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Ingresar el 1er numero");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresar el 2do numero");
+int numero2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresar la operacion 'sumar - restar - mutiplicar - dividir'");
+string operacion = Console.ReadLine();
+int total = 0;
+
+switch(operacion)
+{
+    case "sumar":
+        total = numero1 + numero2;
+        break;
+
+    case "restar":
+        total = numero1 - numero2;
+        break;
+
+    default:
+        Console.WriteLine("No tengo esa operacion programada...");
+        break;
+
+}
+Console.WriteLine("El total es " + total);
+//Console.ReadLine();
