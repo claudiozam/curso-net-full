5ad9d51e56b08902a97f9ff232b07c6f7cb4da9e
claudio z <claudio.zamoszczyk@gmail.com>
2023-05-12 11:44:41 -0300

    nuevas clases para motrar que es la herencia

---
+++ ConsoleAppHolaMundo/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHolaMundo
+{
+    internal class Persona
+    {
+        private string nombre;
+
+        public void Saludar()
+        {
+
+        }
+    }
+}
