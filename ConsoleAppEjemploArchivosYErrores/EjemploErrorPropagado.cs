3bcff275706e44a6b38c360b9f87545f7fb05436
claudio z <claudio.zamoszczyk@gmail.com>
2023-05-24 10:45:45 -0300

    ejemplo completo de log4net

--- ConsoleAppEjemploArchivosYErrores/EjemploErrorPropagado.cs
+++ ConsoleAppEjemploArchivosYErrores/EjemploErrorPropagado.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace ConsoleAppEjemploArchivosYErrores
 {
     internal class EjemploErrorPropagado
     {
+        private static readonly ILog log = LogManager.GetLogger("EjemploErrorPropagado.cs");
 
         public void HacerAlgoConError()
         {
+            log.Debug("Ejecutando HacerAlgoConError.....");
             string[] nombres = new string[2];
             nombres[99] = "Pedro";
             string texto = null;
