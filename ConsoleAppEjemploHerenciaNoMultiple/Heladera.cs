84a39c12f0610c8e6f55c39b560f67d460800376
claudio z <claudio.zamoszczyk@gmail.com>
2023-06-06 16:13:54 -0300

    ejemplo de interfaces

---
+++ ConsoleAppEjemploHerenciaNoMultiple/Heladera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEjemploHerenciaNoMultiple
+{
+    internal class Heladera : Producto, IElectrodomestico
+    {
+        public void HacerAlgoConHeladera()
+        {
+            Console.WriteLine("Ejecutando HacerAlgoConHeladera");
+        }
+
+        public void PrenderElectrodomestico()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
